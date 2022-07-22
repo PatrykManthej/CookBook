0e725b50f43bee8068ecfe8b7bb4b1ce4d8e4edc
manpat <manpat1997@gmail.com>
2022-07-22 09:12:17 +0200

    integration tests refactor

---
+++ CookBook.Tests/IntegrationTests/RecipeToFileManagerIntegrationTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CookBook.Tests.IntegrationTests
+{
+    public class RecipeToFileManagerIntegrationTests
+    {
+    }
+}
---
+++ CookBook.Tests/IntegrationTests/TagManagerIntegrationTests.cs
@@ -0,0 +1,45 @@
+﻿using CookBook.App.Abstract;
+using CookBook.App.Concrete;
+using CookBook.App.Managers;
+using CookBook.Domain.Entity;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace CookBook.Tests.IntegrationTests
+{
+    public class TagManagerIntegrationTests
+    {
+        private static List<Tag> tags = new List<Tag>()
+            {
+                new Tag(1, "obiad"),
+                new Tag(2, "dania z piekarnika"),
+                new Tag(3, "cebula"),
+                new Tag(4, "kotlet"),
+                new Tag(5, "drób"),
+                new Tag(6, "sałatki"),
+                new Tag(7, "ciasto"),
+                new Tag(8, "wege"),
+                new Tag(9, "serniki"),
+            };
+
+        [Fact]
+        public void TagsSeed_WithProperListOfTags_CanAddListOfTags()
+        {
+            //Arrange
+            IService<Tag> tagService = new TagService();
+            var manager = new TagManager(tagService, new ConsoleWrapper());
+
+            //Act
+            manager.TagsSeed(tags);
+            var seededTags = tagService.Items;
+
+            //Assert
+            bool areEqualLists = Enumerable.SequenceEqual(seededTags, tags);
+            areEqualLists.Should().BeTrue();
+        }
+    }
+}
